099f39ac626b1c1a2ba5ee03475b99150dd9adb2
Kavin M R <154052561+Kavinmr62@users.noreply.github.com>
2025-05-28 07:43:55 +0530

    Create magical.java

---
+++ magical.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{    
+public static void main(String[] args) 
+{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    if (n % 9 == 1)
+        System.out.printf("Magic Number");
+    else
+        System.out.printf("Not a Magic Number");
+}
+}
